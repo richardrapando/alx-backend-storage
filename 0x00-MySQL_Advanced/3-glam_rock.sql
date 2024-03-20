36e238604781b7a2e62d63023a6eceb8836f00c8
richardrapando <richardrapando@gmail.com>
2024-03-20 01:33:13 -0700

    MySQL advanced

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,5 @@
+-- SQL script that lists all bands with Glam rock, ranked by their longevity
+SELECT band_name, (IFNULL(split, '2022') - formed) AS lifespan
+    FROM metal_bands
+    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+    ORDER BY lifespan DESC;
